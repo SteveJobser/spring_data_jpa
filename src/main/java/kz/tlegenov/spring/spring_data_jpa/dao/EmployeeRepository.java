e9185c157580369a8b5df32fe258b8fbeb3e019c
SteveJobser <tima_tleg@mail.ru>
2021-04-23 11:35:41 +0600

    Initial commit

---
+++ src/main/java/kz/tlegenov/spring/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package kz.tlegenov.spring.spring_data_jpa.dao;
+
+
+
+
+
+import kz.tlegenov.spring.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
